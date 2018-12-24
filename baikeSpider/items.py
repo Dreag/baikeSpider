26058ae5c61da022076fbc1364cc9d4c7827b67f
Dreag <yang_g1@126.com>
2018-12-25 07:51:08 +0800

    add parse_item

--- baikeSpider/items.py
+++ baikeSpider/items.py
@@ -10,5 +10,13 @@ import scrapy
 
 class BaikespiderItem(scrapy.Item):
     # define the fields for your item here like:
+    original_page = scrapy.Field()
+    original_page_title = scrapy.Field()
+    # id = scrapy.Field()
     # name = scrapy.Field()
-    pass
+    # height = scrapy.Field()
+    # birthPlace = scrapy.Field()
+    # birthday = scrapy.Field()
+    # nationality = scrapy.Field()
+    # relation = scrapy.Field()
+    # basicInformation = scrapy.Field()
--- baikeSpider/pipelines.py
+++ baikeSpider/pipelines.py
@@ -5,7 +5,18 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import json
+from scrapy.exporters import JsonItemExporter
+
 
 class BaikespiderPipeline(object):
     def process_item(self, item, spider):
+        return item
+
+
+class DataSubmitJsonFile(object):
+
+    def process_item(self, item, spider):
+        with open("data/" + item['original_page_title'] + ".txt", 'wb' ) as f:
+            f.write(bytes(item))
         return item
